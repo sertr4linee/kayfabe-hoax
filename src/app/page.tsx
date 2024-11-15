d15a6fe4fcf6e8eb9a144a5a573e06a5894f9d60
cmoi <menzo.smit-adam@epitech.eu>
2024-11-16 00:06:01 +0100

    fix time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
